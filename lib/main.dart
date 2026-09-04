9c5e1e184281fb42c6fef21b3d08f81b7c3aceba
mgandali84-hue <mgandali84@gmail.com>
2026-09-04 22:46:30 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-
 import 'dart:convert';
 import 'dart:async';
-import 'dart:math' as math;
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
